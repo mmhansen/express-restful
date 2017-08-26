8022f2a73d7fa7cbf21e3babe8f507ac085c6c78
mmhansen <hansen.mmb@gmail.com>
2017-08-25 20:56:39 -0400

    add_resource method

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const express = require('express')
+
 
 class Resource {
   constructor() {
@@ -8,8 +10,25 @@ class Resource {
 class Api {
   constructor(app) {
     this.app = app;
+    app.listen(3000)
   }
-  add_resource(resource, path) {
-    
+  add_resource(resource, url) {
+    const self = this
+
+    const t = Object.getOwnPropertyNames(resource.prototype)
+    const allowed = ['get', 'post', 'put', 'delete']
+    const methods = t.filter(function (s) {
+      return allowed.indexOf(s) > -1
+    })
+    methods.map(function (method) {
+      self.app[method](url, resource.prototype[method])
+    })
   }
+}
+
+
+
+module.exports = {
+  Api: Api,
+  Resource: Resource
 }
